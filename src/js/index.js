8d858ea44fb01fc67e7106a6ced52f26781447b0
MadeReihan <reihanmade@gmail.com>
2022-03-19 23:18:42 +0700

    skills, projects

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+
+
 let loader = document.querySelector('.preloader')
 window.addEventListener('load', vanish);
 
@@ -14,10 +16,6 @@ let animation = bodymovin.loadAnimation({
     path: 'https://assets10.lottiefiles.com/packages/lf20_bpqri9y8.json'
 })
 
-function Menu(e){
-    let list = document.querySelector('ul');
-    e.name === 'menu' ? (e.name = "close",list.classList.add('top-[80px]') , list.classList.add('opacity-100')) :( e.name = "menu" ,list.classList.remove('top-[80px]'),list.classList.remove('opacity-100'))
-}
   
 function scrollHeader(){
     const header = document.getElementById('header')
@@ -32,3 +30,75 @@ function scrollHeader(){
 }
 window.addEventListener('scroll',scrollHeader)
 
+
+// drag
+interact('.draggable')
+  .draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'parent',
+        endOnly: true
+      })
+    ],
+    // enable autoScroll
+    autoScroll: true,
+
+    listeners: {
+      // call this function on every dragmove event
+      move: dragMoveListener,
+
+      // call this function on every dragend event
+      end (event) {
+        var textEl = event.target.querySelector('.loaalala')
+
+        textEl && (textEl.textContent =
+          'moved a distance of ' +
+          (Math.sqrt(Math.pow(event.pageX - event.x0, 1) +
+                     Math.pow(event.pageY - event.y0, 1) | 0))
+            .toFixed(2) + 'px')
+      }
+    }
+  })
+
+function dragMoveListener (event) {
+  var target = event.target
+  // keep the dragged position in the data-x/data-y attributes
+  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+  // translate the element
+  target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+
+  // update the posiion attributes
+  target.setAttribute('data-x', x)
+  target.setAttribute('data-y', y)
+}
+
+// this function is used later in the resizing and gesture demos
+window.dragMoveListener = dragMoveListener
+
+
+
+// mixitup filter
+let mixerPortfolio = mixitup('.work__container', {
+    selectors:{
+        target:'.work__card'
+    },
+    animation:{
+        duration:300
+    }
+})
+
+/* Link active work */ 
+
+const linkWork = document.querySelectorAll('.work__item')
+
+function activeWork(){
+    linkWork.forEach(l=>l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+
+linkWork.forEach(l=>l.addEventListener('click',activeWork))

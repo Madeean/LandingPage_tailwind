4b88420353857373161fe4fcc510abe99de55f0f
MadeReihan <reihanmade@gmail.com>
2022-03-20 22:01:03 +0700

    dark theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 module.exports = {
+  darkMode: 'class',
   content: ['./index.html'],
   theme: {
     extend: {
       boxShadow:{
         card: "0px 4px 30px -1px rgba(0,0,0,0.07)"
+      },
+      colors:{
+        dakitam: "rgb(11,17,30)",
+        dakitih: "rgb(240,242,244)",
+        dakibu: "rgb(21,28,40)"
       },
       
     },

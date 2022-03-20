adf4e66ea9ced1408c46543d6296a9a77a6b2309
MadeReihan <reihanmade@gmail.com>
2022-03-20 16:27:27 +0700

    stmt

--- src/js/index.js
+++ src/js/index.js
@@ -120,7 +120,9 @@ function sendEmail(){
     To : 'madereihan26@gmail.com',
     From : `${document.getElementById("email").value}`,
     Subject : "new value from contact form portfolio website",
-    Body : "dawdadasdaw"
+    Body : `Name: ${document.getElementById("name").value} <br>
+            Email: ${document.getElementById("email").value} <br>
+            Message: ${document.getElementById("message").value}`
   }).then(
   message => alert(message)
   );
